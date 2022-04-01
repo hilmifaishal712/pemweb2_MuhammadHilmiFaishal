d1ab25a5a19bbe58617ee1da6567a7c6539bcfea
hilmifaishal712 <92616737+hilmifaishal712@users.noreply.github.com>
2022-04-01 22:21:30 +0700

    Add files via upload

---
+++ praktikum5/bank.php
@@ -0,0 +1,31 @@
+<?php
+require_once './class_accout.php';
+
+class AccountBank extends Account
+{
+    public function doTransfer($destination, $uangTranfer)
+    {
+        Account::withdraw($uangTranfer, false);
+        $destination->deposit($uangTranfer, false);
+
+        echo "Transfer sejumlah " .  number_format($uangTranfer, 2, ',', '.') . " dari $this->nama - $this->nomor ke $destination->nama - $destination->nomor sukses! </br>";
+    }
+}
+
+$azzam = new AccountBank('ABC0010', 'Azzam', 3450000);
+$anggi = new AccountBank('ABC0011', 'Anggi', 45000);
+$hilmi = new AccountBank('ABC0012', 'Hilmi', 235000);
+
+$azzam->deposit(1000000);
+$azzam->cetak();
+
+$azzam->doTransfer($anggi, 1000000);
+$azzam->cetak();
+
+$azzam->doTransfer($hilmi, 500000);
+$azzam->cetak();
+
+$anggi->cetak();
+$hilmi->cetak();
+
+?>
