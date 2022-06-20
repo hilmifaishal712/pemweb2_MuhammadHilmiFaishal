9a2fe69b035cbb3b25162a7a6237e43ef817e3d8
hilmifaishal712 <92616737+hilmifaishal712@users.noreply.github.com>
2022-06-20 09:17:56 +0700

    Update head.php

--- praktikum8/application/views/layout/head.php
+++ praktikum8/application/views/layout/head.php
@@ -9,8 +9,11 @@
 
     <style>
         .utama {
-            background: rgb(27, 0, 36);
-            background: linear-gradient(90deg, rgba(27, 0, 36, 1) 0%, rgba(135, 38, 167, 1) 0%, rgba(0, 212, 255, 1) 100%);
+            background: #8360c3;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #2ebf91, #8360c3);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #2ebf91, #8360c3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
         }
 
         .pesan {
